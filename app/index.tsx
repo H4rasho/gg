03277e68387a467def0918cacdf2fefb5f373cd6
Isaias Lorca <isaias.lorca13@gmail.com>
2024-06-29 21:15:09 -0400

    Add a Button

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,9 @@
-import { Text, View } from "react-native";
+import { Text, View, Pressable } from "react-native";
 
 export default function Index() {
+  function onPressFunction () {
+    console.log("Jeh")
+  }
   return (
     <View
       style={{
@@ -10,6 +13,19 @@ export default function Index() {
       }}
     >
       <Text>Control de gastos</Text>
+      <Pressable onPressIn={onPressFunction}
+      style={{
+        top:20,
+        borderRadius: 10 ,
+        backgroundColor: "black",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 100,
+        height: 50,
+      }}
+      >
+        <Text style={{fontSize:30, color: "white"}}>+</Text>
+      </Pressable>
     </View>
   );
 }
